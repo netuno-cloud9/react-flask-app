590287127ef59b3a54924bdacf698b9e00af4631
Paulo Victor <110955443+netuno-cloud9@users.noreply.github.com>
2024-06-24 00:48:50 -0300

    Update Navbar.js

--- Navbar.js
+++ Navbar.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ handleLogout }) => {
   return (
     <nav style={styles.navbar}>
+      
       <ul style={styles.navList}>
-        <li style={styles.navItem}><Link to="/" style={styles.navLink}>Home</Link></li>
+        <li style={styles.navItem}><Link to="/Home" style={styles.navLink}>Home</Link></li>
         <li style={styles.navItem}><Link to="/about" style={styles.navLink}>About</Link></li>
         <li style={styles.navItem}><Link to="/profile" style={styles.navLink}>Profile</Link></li>
         <li style={styles.navItem}><Link to="/settings" style={styles.navLink}>Settings</Link></li>
+        <img src="logositesenai.png" alt="Logo" style={styles.logo} /> {/* Replace with your logo image */}
         <li style={styles.navItem}>
           <button onClick={handleLogout} style={styles.logoutButton}>Logout</button>
         </li>
@@ -22,11 +24,17 @@ const styles = {
     backgroundColor: '#2196f3',  // Blue color
     padding: '10px 20px',
     marginBottom: '20px',
+    display: 'flex',
+    alignItems: 'center', // Center items vertically in the navbar
+    justifyContent: 'space-between', // Distribute items evenly across the navbar
+  },
+  logo: {
+    width: '50px', // Adjust width as needed
+    height: 'auto', // Maintain aspect ratio
   },
   navList: {
     listStyleType: 'none',
     display: 'flex',
-    justifyContent: 'flex-start',
     alignItems: 'center',  // Align items vertically in the navbar
     padding: 0,
     margin: 0,

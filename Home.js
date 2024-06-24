ad8cdc3bc2e086d52904dad912e2c4ce0ac2644f
Paulo Victor <110955443+netuno-cloud9@users.noreply.github.com>
2024-06-24 00:24:36 -0300

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import Navbar from './Navbar';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 
 const Home = () => {
   const navigate = useNavigate();
@@ -27,12 +26,48 @@ const Home = () => {
     }
   };
 
+  // Array of fast links with images and texts
+  const fastLinks = [
+    {
+      url: 'https://www.portaldaindustria.com.br/senai/canais/grand-prix-de-inovacao/',
+      imageSrc: 'logogp.png',
+      text: 'Grand Prix',
+    },
+    {
+      url: 'https://edu.google.com/intl/ALL_br/workspace-for-education/classroom/',
+      imageSrc: 'logoclassroom.png',
+      text: 'Classroom',
+    },
+    {
+      url: 'https://senaiweb.fieb.org.br/areadocente/',
+      imageSrc: 'logoportaldoc.png',
+      text: 'Portal Docente',
+    },
+    {
+      url: 'https://ead.sesieducacao.com.br/uc/login?dir=%2Fuc%2F&hash=portal',
+      imageSrc: 'logounidus.png',
+      text: 'Unindústria',
+    },
+  ];
+
   return (
     <div>
       <Navbar handleLogout={handleLogout} />
       <main style={styles.main}>
         <h1 style={styles.heading}>Home Page</h1>
         <p style={styles.content}>Bem-vindo à Home Page!</p>
+
+        {/* Fast links section */}
+        <section style={styles.fastLinks}>
+          {fastLinks.map((link, index) => (
+            <Link key={index} to={link.url} style={styles.link}>
+              <img src={link.imageSrc} alt={link.text} style={styles.image} />
+              <p>{link.text}</p>
+            </Link>
+          ))}
+        </section>
+
+        {/* Carousel section */}
         <Carousel
           style={styles.carousel}
           interval={3000} // Set interval for automatic sliding
@@ -42,11 +77,11 @@ const Home = () => {
           autoPlay={true} // Ensure autoPlay is enabled
           stopOnHover={false} // Ensure the carousel does not stop on hover
         >
-          <Link to="https://catalogo.tecnicosenai.com.br/">
+          <a href="https://catalogo.tecnicosenai.com.br/" target="_blank" rel="noopener noreferrer">
             <div>
               <img src="https://www.sc.senai.br/sites/default/files/inline-images/Banners-rotativos-CP-EAD-1366x382.jpg" alt="Slide 1" />
             </div>
-          </Link>
+          </a>
           <Link to="/slide2">
             <div>
               <img src="https://www.sc.senai.br/sites/default/files/inline-images/Banners-rotativos-SENAI-MAIS-CARREIRA-1366x382.jpg" alt="Slide 2" />
@@ -58,6 +93,7 @@ const Home = () => {
             </div>
           </Link>
         </Carousel>
+
         <p style={styles.content}>Aqui você pode adicionar mais conteúdo e funcionalidades.</p>
       </main>
       <footer style={styles.footer}>
@@ -85,6 +121,22 @@ const styles = {
   carousel: {
     width: '80%',
     margin: '0 auto 20px auto',
+  },
+  fastLinks: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    marginTop: '20px',
+  },
+  link: {
+    textDecoration: 'none',
+    color: '#333',
+    textAlign: 'center',
+  },
+  image: {
+    width: '100px', // Adjust size as needed
+    height: 'auto', // Maintain aspect ratio
+    marginBottom: '10px', // Added margin bottom for spacing
   },
   footer: {
     textAlign: 'center',
